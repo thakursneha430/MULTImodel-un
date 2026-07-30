a3420105c1acf8ab7e1b4f50822299b94a1fc96c
thakursneha430 <thakursneha4300@gmaul.com>
2026-07-30 21:39:17 +1245

    image,text,pdf loader added

--- MULTImodel-un/test_pipeline.py
+++ MULTImodel-un/test_pipeline.py
@@ -133,3 +133,51 @@ if __name__ == "__main__":
 
     # Test Text File
     test_document("data/raw/sample.txt")
+from src.ingestion.ingestion_pipeline import IngestionPipeline
+
+
+def test_document(file_path: str):
+
+    print("=" * 60)
+    print("MULTIMODAL DOCUMENT INTELLIGENCE")
+    print("DOCUMENT INGESTION PIPELINE")
+    print("=" * 60)
+
+    print(f"\nInput File: {file_path}")
+
+    pipeline = IngestionPipeline(
+        file_path
+    )
+
+    document = pipeline.run()
+
+    print("\n" + "=" * 60)
+    print("INGESTION SUCCESSFUL")
+    print("=" * 60)
+
+    print("\nDocument ID:")
+    print(document.document_id)
+
+    print("\nFile Name:")
+    print(document.file_name)
+
+    print("\nFile Type:")
+    print(document.file_type)
+
+    print("\nMetadata:")
+    print(document.metadata)
+
+    print("\nExtracted Content:")
+    print("-" * 60)
+
+    if document.content:
+        print(document.content)
+    else:
+        print("No text extracted.")
+
+
+if __name__ == "__main__":
+
+    test_document(
+        "data/raw/sample.jpg"
+    )
